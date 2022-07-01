28e1a75ebfef310e04ffe64f50074aa5139d524a
shawn.mathews <shawn.mathews@stmartin.edu>
2022-07-01 08:55:52 -0700

    validation

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -7,11 +7,48 @@ mongoose
 	.catch((error) => console.log('Could not connect to MongoDB...', error));
 
 const courseSchema = new mongoose.Schema({
-	name: String,
+	name: {
+		type: String,
+		required: true,
+		minLength: 5,
+		maxLength: 255,
+		//match:
+	},
+	category: {
+		type: String,
+		required: true,
+		enum: ['web', 'mobile', 'network'],
+		lowercase: true,
+		trim: true,
+	},
 	author: String,
-	tags: [String],
+	tags: {
+		type: Array,
+		validate: {
+			isAsync: true,
+			validator: function (v, callback) {
+				return new Promise((callback) => {
+					setTimeout(() => {
+						const result = v && v.length > 0;
+						callback(result);
+					}, 1000);
+				});
+			},
+			message: 'A course should have at least one tag',
+		},
+	},
 	date: { type: Date, default: Date.now },
 	isPublished: Boolean,
+	price: {
+		type: Number,
+		required: function () {
+			return this.isPublished;
+		},
+		min: 10,
+		max: 200,
+		get: (v) => Math.round(v),
+		set: (v) => Math.round(v),
+	},
 });
 
 const Course = mongoose.model('Course', courseSchema);
@@ -19,13 +56,21 @@ const Course = mongoose.model('Course', courseSchema);
 async function createCourse() {
 	const course = new Course({
 		name: 'Angular Course',
+		category: 'Web',
 		author: 'Mosh',
-		tags: ['angular', 'frontend'],
+		tags: ['frontend'],
 		isPublished: true,
+		price: 15.8,
 	});
 
-	const result = await course.save();
-	console.log(result);
+	try {
+		const result = await course.save();
+		console.log(result);
+	} catch (err) {
+		for (let field in err.errors) {
+			console.log(err.errors[field].message);
+		}
+	}
 }
 
 async function getCourses() {
@@ -48,13 +93,13 @@ async function getCourses() {
 		// .or([{ author: 'Mosh' }, { isPublished: true }])
 		// .find({ author: /^Mosh/})
 		// .find({ author: /Hamedani$/i })
-		.find({ author: /.*Mosh.*/i })
-		.skip((pageNumber - 1) * pageSize)
-		.limit(pageSize)
+		.find({ _id: '62bf10b67638e74e7a6b45a2' })
+		// .skip((pageNumber - 1) * pageSize)
+		// .limit(pageSize)
 		.sort({ name: 1 })
-		// .select({ name: 1, tags: 1 });
-		.count();
-	console.log(courses);
+		.select({ name: 1, tags: 1, price: 1 });
+	// .count();
+	console.log(courses[0].price);
 }
 
 async function updateCourse(id) {
@@ -71,4 +116,4 @@ async function updateCourse(id) {
 	console.log(result);
 }
 
-updateCourse('5a68fde3f09ad7646ddec17e');
+getCourses();
