8da5424caa467d004bcb795eaa708a72971be3b0
shawn.mathews <shawn.mathews@stmartin.edu>
2022-07-01 13:11:52 -0700

    persistence exercise

--- vidly-app/index.js
+++ vidly-app/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
+const mongoose = require('mongoose');
+
+mongoose
+	.connect('mongodb://127.0.0.1/vidly')
+	.then(() => console.log('Connected to MongoDB...'))
+	.catch((error) => console.log('Could not connect to MongoDB...', error));
 
 app.use(express.json());
 
